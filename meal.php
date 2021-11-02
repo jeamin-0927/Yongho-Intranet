86d157ff0f1a09b862fbc4a7a61794e446ad42d5
최재민 <colajeamin@gmail.com>
2021-11-02 22:08:23 +0900

    edit getWebPage

--- meal.php
+++ meal.php
@@ -1,15 +1,5 @@
 ﻿<?php
-    function getWebPage($url)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        $res = curl_exec($ch);
-        curl_close($ch);
-         
-        return $res;
-    }
+    include 'common.php';
     $dt = $_GET['date'];
     if($dt == ''){
         $dt = date("Ymd");
